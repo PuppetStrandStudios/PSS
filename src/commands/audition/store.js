062a9382b5ad7387de81db661a6990dc5e7f94bf
PuppetStrandStudios <puppetstrandstudios@gmail.com>
2026-06-19 22:37:45 +0100

    Delete src/commands/audition/store.js

--- src/commands/audition/store.js
+++
@@ -1,6 +0,0 @@
-// ============================================
-// SHARED AUDITION STORE
-// File goes in: src/commands/audition/store.js
-// ============================================
-
-export const activeAuditions = new Map();
