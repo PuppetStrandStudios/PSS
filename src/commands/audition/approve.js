f46f6cfa664177bc1f1adc4bc697af2e34f5f535
PuppetStrandStudios <puppetstrandstudios@gmail.com>
2026-06-19 22:38:42 +0100

    Update approve.js

--- src/commands/audition/approve.js
+++ src/commands/audition/approve.js
@@ -2,7 +2,7 @@ import { SlashCommandBuilder, MessageFlags } from 'discord.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 import { successEmbed, createEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
-import { activeAuditions } from './store.js';
+import { activeAuditions } from '../../utils/store.js';
 import { logToChannel } from './audition.js';
 
 const APPROVED_MESSAGE = (time) =>
